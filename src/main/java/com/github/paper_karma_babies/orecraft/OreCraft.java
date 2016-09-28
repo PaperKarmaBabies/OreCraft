bf711dc84fb2804b9f0e9c205a99c19b07fb17fd
Lukas Tenbrink <lukastenbrink@googlemail.com>
2016-09-28 16:52:04 +0200

    Add logger.

--- src/main/java/com/github/paper_karma_babies/orecraft/OreCraft.java
+++ src/main/java/com/github/paper_karma_babies/orecraft/OreCraft.java
@@ -3,6 +3,7 @@ package com.github.paper_karma_babies.orecraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Main entry point for OreCraft.
@@ -19,9 +20,13 @@ public class OreCraft
     public static final String MODID = "orecraft";
     public static final String VERSION = "@VERSION@";
 
+    public static Logger logger;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         System.out.println(MOD_NAME + " is alive!");
+
+        logger = event.getModLog();
     }
 }
